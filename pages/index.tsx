a04dcf7399d3e6bd1f2b06d643fd4c62222fee9d
Hugh Rawlinson <hughr2005@gmail.com>
2022-11-12 13:24:11 +0100

    Update for Next 13

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,32 +19,32 @@ const Home: NextPage = () => {
         <p className={styles.description}>Strictly for touring purposes only</p>
 
         <div className={styles.grid}>
-          <Link passHref href="/cycles">
-            <a className={styles.card}>
-              <h2>Cycles &rarr;</h2>
-              <p>Look at a list of the cycles I&apos;ve done</p>
-            </a>
+          <Link passHref href="/cycles" className={styles.card}>
+
+            <h2>Cycles &rarr;</h2>
+            <p>Look at a list of the cycles I&apos;ve done</p>
+
           </Link>
 
-          <Link passHref href="/cycles">
-            <a className={styles.card}>
-              <h2>...still Cycles &rarr;</h2>
-              <p>That&apos;s the only page on the website</p>
-            </a>
+          <Link passHref href="/cycles" className={styles.card}>
+
+            <h2>...still Cycles &rarr;</h2>
+            <p>That&apos;s the only page on the website</p>
+
           </Link>
 
-          <Link passHref href="/cycles">
-            <a className={styles.card}>
-              <h2>No, seriously</h2>
-              <p>There aren&apos;t any other pages my friend</p>
-            </a>
+          <Link passHref href="/cycles" className={styles.card}>
+
+            <h2>No, seriously</h2>
+            <p>There aren&apos;t any other pages my friend</p>
+
           </Link>
 
-          <Link passHref href="/cycles">
-            <a className={styles.card}>
-              <h2>Didn&apos;t like any of the other links?</h2>
-              <p>Ok, click this one.</p>
-            </a>
+          <Link passHref href="/cycles" className={styles.card}>
+
+            <h2>Didn&apos;t like any of the other links?</h2>
+            <p>Ok, click this one.</p>
+
           </Link>
         </div>
       </main>
